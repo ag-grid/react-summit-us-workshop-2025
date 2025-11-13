12129955cff92076a9fdbff1c442095ac3b8dc13
James Swinton-Bland <james_swinton@hotmail.co.uk>
2025-11-13 09:15:57 +0000

    add solution

--- src/challenges/challenge-4/EnterpriseChart.tsx
+++ src/challenges/challenge-4/EnterpriseChart.tsx
@@ -4,8 +4,53 @@ import 'ag-charts-enterprise';
 import type { AgChartOptions } from 'ag-charts-enterprise';
 
 export default function EnterpriseChart() {
-  // TODO: Chart Options
-  const options: AgChartOptions = {};
+  const options: AgChartOptions = {
+    data: data,
+    title: { text: `Global CO2 Emissions since 154AD` },
+    animation: { enabled: false },
+    zoom: {
+      enabled: true,
+      axes: 'x',
+      anchorPointX: 'pointer',
+      anchorPointY: 'pointer',
+      autoScaling: {
+        enabled: true,
+      },
+    },
+    navigator: {
+      enabled: true,
+      miniChart: {
+        enabled: true,
+      },
+    },
+    series: [
+      {
+        type: 'line',
+        xKey: 'Year AD',
+        yKey: 'CO2 spline (20 yr, ppm)',
+        marker: {
+          enabled: false,
+        },
+      },
+    ],
+    contextMenu: {
+      enabled: true,
+    },
+    axes: [
+      {
+        type: 'number',
+        position: 'left',
+        crosshair: {
+          enabled: true,
+        },
+      },
+      {
+        type: 'category',
+        position: 'bottom',
+        crosshair: { enabled: true },
+      },
+    ],
+  };
 
   return (
     <AgCharts style={{ width: '100%', height: '100%' }} options={options} />
