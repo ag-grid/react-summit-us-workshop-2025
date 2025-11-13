743e505da3f6255c409bbdfacf200d4074462e6b
James Swinton-Bland <james_swinton@hotmail.co.uk>
2025-11-13 12:44:09 +0000

    update solution

--- src/challenges/challenge-6/FinancialChart.tsx
+++ src/challenges/challenge-6/FinancialChart.tsx
@@ -8,113 +8,168 @@ import 'ag-charts-enterprise';
 import { getData } from './data.ts';
 
 const chartAnnotations = [
-  // Global Financial Crisis - September 2008
-  {
-    type: 'callout',
-    text: 'Global Financial Crisis',
-    color: '#040404',
-    fill: '#ff6666',
-    fillOpacity: 0.7,
-    stroke: '#F23645',
-    strokeOpacity: 1,
-    strokeWidth: 2,
-    start: {
-      x: {
-        __type: 'date',
-        value: new Date('2008-09-15').getTime(),
-      },
-      y: 1251,
-    },
-    end: {
-      x: {
-        __type: 'date',
-        value: new Date('2007-10-01').getTime(),
-      },
-      y: 1450,
-    },
-  },
-
-  // GFC Market Bottom - March 2009
-  {
-    type: 'comment',
-    text: 'Market Bottom',
-    x: {
-      __type: 'date',
-      value: new Date('2009-03-09').getTime(),
-    },
-    y: 676,
-  },
-  {
-    type: 'horizontal-line',
-    value: 676,
-    stroke: '#089981',
-    strokeWidth: 1,
-    lineStyle: 'dotted',
-    axisLabel: {
-      fill: '#089981',
-    },
-  },
-
-  // COVID-19 Pandemic - March 2020
-  {
-    type: 'callout',
-    text: 'COVID-19 Pandemic',
-    color: '#040404',
-    fill: '#ff9999',
-    fillOpacity: 0.6,
-    stroke: '#F23645',
-    strokeOpacity: 1,
-    strokeWidth: 2,
-    start: {
-      x: {
-        __type: 'date',
-        value: new Date('2020-03-16').getTime(),
-      },
-      y: 2386,
-    },
-    end: {
-      x: {
-        __type: 'date',
-        value: new Date('2020-01-15').getTime(),
-      },
-      y: 3100,
-    },
-  },
-
-  // COVID Market Bottom
   {
     type: 'vertical-line',
-    value: new Date('2020-03-23').getTime(),
-    stroke: '#F23645',
+    text: {
+      label: 'Global Financial Crisis',
+      position: 'top',
+      alignment: 'center',
+      fontSize: 14,
+      fontFamily: 'Verdana, sans-serif',
+      color: '#dc5050',
+    },
+    value: {
+      value: {
+        __type: 'date',
+        value: '2008-09-16T23:00:00.000Z',
+      },
+      groupPercentage: 0.110887833394141,
+    },
+    handle: {
+      strokeOpacity: 1,
+      strokeWidth: 2,
+      fill: 'white',
+    },
+    axisLabel: {
+      enabled: true,
+      cornerRadius: 2,
+      stroke: '#dc5057',
+      strokeOpacity: 1,
+      fill: '#dc5057',
+      fillOpacity: 1,
+      fontSize: 12,
+      fontFamily: 'Verdana, sans-serif',
+      color: 'white',
+    },
+    stroke: '#dc5057',
+    strokeOpacity: 1,
     strokeWidth: 1,
     lineStyle: 'dashed',
   },
-
-  // Post-COVID Recovery
-  {
-    type: 'text',
-    text: 'Recovery Rally',
-    x: {
-      __type: 'date',
-      value: new Date('2020-11-01').getTime(),
-    },
-    y: 3500,
-  },
-
-  // 2022 Bear Market
-  {
-    type: 'comment',
-    text: '2022 Bear Market',
-    x: {
-      __type: 'date',
-      value: new Date('2022-06-16').getTime(),
-    },
-    y: 3666,
-  },
   {
     type: 'vertical-line',
-    value: new Date('2022-06-16').getTime(),
-    stroke: '#F23645',
+    text: {
+      label: 'Covid',
+      position: 'top',
+      alignment: 'center',
+      fontSize: 14,
+      fontFamily: 'Verdana, sans-serif',
+      color: '#951c81',
+    },
+    value: {
+      value: {
+        __type: 'date',
+        value: '2020-02-07T00:00:00.000Z',
+      },
+      groupPercentage: 0.560284983558314,
+    },
+    handle: {
+      strokeOpacity: 1,
+      strokeWidth: 2,
+      fill: 'white',
+    },
+    axisLabel: {
+      enabled: true,
+      cornerRadius: 2,
+      stroke: '#951c81',
+      strokeOpacity: 1,
+      fill: '#951c81',
+      fillOpacity: 1,
+      fontSize: 12,
+      fontFamily: 'Verdana, sans-serif',
+      color: 'white',
+    },
+    stroke: '#951c81',
+    strokeOpacity: 1,
+    strokeWidth: 1,
+    lineStyle: 'dashed',
+  },
+  {
+    type: 'arrow',
+    text: {
+      label: 'Quantitative Easing',
+      position: 'center',
+      alignment: 'center',
+      fontSize: 12,
+      fontFamily: 'Verdana, sans-serif',
+      color: '#128f42',
+    },
+    stroke: '#128f42',
+    strokeOpacity: 1,
+    strokeWidth: 1,
+    lineStyle: 'dashed',
+    start: {
+      x: {
+        value: {
+          __type: 'date',
+          value: '2020-07-22T23:00:00.000Z',
+        },
+        groupPercentage: 0.5045670442092546,
+      },
+      y: 2602.6272577996715,
+    },
+    end: {
+      x: {
+        value: {
+          __type: 'date',
+          value: '2021-10-27T23:00:00.000Z',
+        },
+        groupPercentage: 0.21227621483463424,
+      },
+      y: 4171.59277504105,
+    },
+    handle: {
+      strokeOpacity: 1,
+      strokeWidth: 2,
+      fill: 'white',
+    },
+  },
+  {
+    type: 'parallel-channel',
+    height: 661.7405582922825,
+    middle: {
+      strokeWidth: 1,
+      lineDash: [6, 5],
+    },
+    text: {
+      label: 'Steady Recovery Growth',
+      position: 'top',
+      alignment: 'center',
+      fontSize: 14,
+      fontFamily: 'Verdana, sans-serif',
+      color: '#d6b72e',
+    },
+    background: {
+      fill: '#d6b72e',
+      fillOpacity: 0.2,
+    },
+    start: {
+      x: {
+        value: {
+          __type: 'date',
+          value: '2009-02-23T00:00:00.000Z',
+        },
+        groupPercentage: 0.5,
+      },
+      y: 1268.472906403941,
+    },
+    end: {
+      x: {
+        value: {
+          __type: 'date',
+          value: '2018-12-24T00:00:00.000Z',
+        },
+        groupPercentage: 0.49999999999974015,
+      },
+      y: 2997.5369458128075,
+    },
+    handle: {
+      strokeOpacity: 1,
+      strokeWidth: 2,
+      fill: 'white',
+    },
+    stroke: '#d6b72e',
+    strokeOpacity: 1,
     strokeWidth: 1,
     lineStyle: 'dashed',
   },
