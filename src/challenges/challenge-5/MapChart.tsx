fb3a9f8f782c5a695400739ca646e57ac93e3c11
James Swinton-Bland <james_swinton@hotmail.co.uk>
2025-11-13 09:16:29 +0000

    add solution

--- src/challenges/challenge-5/MapChart.tsx
+++ src/challenges/challenge-5/MapChart.tsx
@@ -7,8 +7,46 @@ import { topology, cables } from './topology.js';
 import { data } from './data.js';
 
 export default function MapChart() {
-  // Chart Options: TODO
-  const chartOptions: AgChartOptions = {};
+  const chartOptions: AgChartOptions = {
+    title: {
+      text: 'Global GDP Heatmap',
+    },
+    subtitle: {
+      text: 'Countries colored by GDP - Click to select/deselect',
+    },
+    data: data,
+    topology,
+    series: [
+      {
+        type: 'map-shape',
+        idKey: 'iso3',
+        topologyIdKey: 'iso3',
+        colorKey: 'gdp_md',
+        colorName: 'GDP per Capita',
+        label: {
+          enabled: true,
+          fontSize: 9,
+        },
+        highlight: {
+          highlightedItem: {
+            strokeWidth: 3,
+            fillOpacity: 0.9,
+          },
+        },
+      },
+    ],
+    gradientLegend: {
+      enabled: true,
+      position: 'bottom',
+      gradient: {
+        preferredLength: 400,
+        thickness: 12,
+      },
+    },
+    zoom: {
+      enabled: true,
+    },
+  };
 
   return (
     <AgCharts
